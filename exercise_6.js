dd14405cd995b70f9a9962437a7f6c0171db9d4f
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-10 16:57:49 -0800

    Create exercise_6.js

---
+++ exercise_6.js
@@ -0,0 +1,22 @@
+// Confirm the Ending
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+
+
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+function confirmEnding(str, target) {
+    return str;
+}
+
+confirmEnding("Bastian", "n");
+
+// function confirmEnding(str, target) {
+//     return str;
+// }
+
+// confirmEnding("Bastian", "n");
+
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
