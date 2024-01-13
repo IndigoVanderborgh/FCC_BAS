d7c9d3d13a012c598ffb70ff410943a5205ca472
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-12 19:23:01 -0800

    Update exercise_5.js

--- exercise_5.js
+++ exercise_5.js
@@ -9,7 +9,7 @@ function largestOfFour(arr) {
     const results = [];
     for (let i = 0; i < arr.length; i++) {
         let largestNumber = arr[i][0];
-        for (let j = 1; j < arr[i].length; j++) {
+        for (let j = 0; j < arr[i].length; j++) {
             if (arr[i][j] > largestNumber) {
                 largestNumber = arr[i][j];
             }
@@ -28,3 +28,21 @@ largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 85
 // largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
+
+// Let's lok at this answer. 
+
+// function largestOfFour(arr) { // name of the function
+//     const results = []; // empty array results
+//     for (let i = 0; i < arr.length; i++) { // simple for loop using the array length, so if the condition is true then move on to line 37
+//         let largestNumber = arr[i][0]; // we make the largestNumber become the first value in the i sub array, so it will update as we go.
+//         for (let j = 0; j < arr[i].length; j++) { // simple loop now for the sub array, we use [i] because its the first sub array still
+//             if (arr[i][j] > largestNumber) { //  Now if the value in the subarray is more than the one previous, it will replace largestNumber. 
+//                 largestNumber = arr[i][j]; // 
+//             } // 
+//         } // 
+//     results[i] = largestNumber;  // 
+//     } // 
+//     return results; // 
+// } // 
+// 
+// 
