f80907e361200e382ffd090f245f43129e07ff54
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-12 17:51:09 -0800

    Update exercise_4.js

--- exercise_4.js
+++ exercise_4.js
@@ -34,3 +34,13 @@ findLongestWordLength("The quick brown fox jumped over the lazy dog");
 // Take the string and convert it into an array of words. Declare a variable to keep track of the maximum length and loop from 0 to the length of the array of words.
 
 // Then check for the longest word by comparing the current word to the previous one and storing the new longest word. At the end of the loop just return the number value of the variable maxLength.
+
+// There are multiple ways to approach this. But let's look at the answer. 
+// let words = str.split(' '); // splits the sentence into indices of an array, where each word takes an index 
+// let maxLength = 0; // initial value for maxLength
+
+// for (let i = 0; i < words.length; i++) { // simple for loop
+//     if (words[i].length > maxLength) { // if the words exceed masLength (stated to be 0) --> see below
+//         maxLength = words[i].length; // maxLength now becomes the length of the word that superceded it. If not, nothing happens.
+//     }
+// }
