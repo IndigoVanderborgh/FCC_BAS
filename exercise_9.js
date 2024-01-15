cb7e4f2b27d3bc2d9f77acc720d59ff361ce78e7
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-14 17:21:56 -0800

    Create exercise_9.js

---
+++ exercise_9.js
@@ -0,0 +1,27 @@
+
+// Finders Keeper
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. 
+// This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+
+
+
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+function findElement(arr, func) {
+    let num = 0;
+    return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+// function findElement(arr, func) {
+//     let num = 0;
+//     return num;
+// }
+
+// findElement([1, 2, 3, 4], num => num % 2 === 0); 
+
+// num % 2 === 0<--- This is to check if the value of n is an even number
+
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
+
+
