85a8a760779282a4977f0e94b9765e6ea4d49986
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-14 17:31:35 -0800

    Create exercise_10.js

---
+++ exercise_10.js
@@ -0,0 +1,20 @@
+// Boo who
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+// ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+function booWho(bool) {
+    return bool;
+}
+
+booWho(null);
+
+// function booWho(bool) {
+//     return bool;
+// }
+
+// booWho(null);
+
+// ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
