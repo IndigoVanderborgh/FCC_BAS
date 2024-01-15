84753b8c0480900adcd2d6ca9b2da7729e4b4b93
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-14 18:48:35 -0800

    Update exercise_7.js

--- exercise_7.js
+++ exercise_7.js
@@ -1,4 +1,4 @@
-// Repeat a String Repeat a String
+// Repeat a String
 
 // Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. 
 
@@ -23,3 +23,19 @@ repeatStringNumTimes("abc", 3);
 // repeatStringNumTimes("abc", 3);
 
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
+// function repeatStringNumTimes(str, num) { <-- function declaration
+//     let accumalatedStr = ""; <-- variable "accumalatedStr" is an empty string, which we will use to update to our new string.
+//     for (let i = 0; i < num; i++) { <-- for loop, to multiple the amount of strings by num
+//         accumalatedStr += str; <-- accumalatedStr = accumalatedStr + str, looped however many times stated by num.
+//     }
+//     return console.log(accumalatedStr);  <-- console logs once condition is complete.
+// }
+
+// repeatStringNumTimes("abc", 3);
+
+
+// Create an empty string variable to store the repeated word.
+// Use a for loop or for loop to repeat code as many times as needed according to num
+// Then we add the string to the variable created on step one inside of the loop.
+// At the end of the loop, return the variable for the repeated word.
+
