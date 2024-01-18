12ab9a15d8ead6e893b5ce570957d75e95770949
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-18 13:17:36 -0800

    Update exercise_11.js

--- exercise_11.js
+++ exercise_11.js
@@ -61,6 +61,15 @@ function titleCase(str) {
 titleCase("I'm a little tea pot");
 
 
+function titleCase(str) {
+    const newTitle = str.split(" ");
+    const updatedTitle = [];
+    for (let i in newTitle) {
+        updatedTitle[i] = newTitle[i][0].toUpperCase() + newTitle[i].slice(1).toLowerCase();
+    }
+    return updatedTitle.join(" ");
+}
+
 
 // GIVEN:
 // function titleCase(str) {
