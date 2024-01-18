5e549b40d798b870456108825d12a70097635a37
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-17 21:17:03 -0800

    Update exercise_11.js

--- exercise_11.js
+++ exercise_11.js
@@ -7,37 +7,73 @@
 
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
 function titleCase(str) {
-    const newTitle =  str.split(" ");
-    const updatedTitle = [];
-    for (let st in newTitle) {
-        updatedTitle[st] = newTitle[st][0].toUppercase() + newTitle[st].slice(1).toLowerCase();
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
     }
-    return updatedTitle.join(" ")
+    return str.join(' ');
+}
+titleCase("I'm a little tea pot");
+
+
+function titleCase(str) {
+    // Step 1. Lowercase the string
+    str = str.toLowerCase();
+    // str = "I'm a little tea pot".toLowerCase();
+    // str = "i'm a little tea pot";
+    
+    // Step 2. Split the string into an array of strings
+    str = str.split(' ');
+    // str = "i'm a little tea pot".split(' ');
+    // str = ["i'm", "a", "little", "tea", "pot"];
+    
+    // Step 3. Create the FOR loop
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    /* Here str.length = 5
+    1st iteration: str[0] = str[0].charAt(0).toUpperCase() + str[0].slice(1);
+                    str[0] = "i'm".charAt(0).toUpperCase()  + "i'm".slice(1);
+                    str[0] = "I"                            + "'m";
+                    str[0] = "I'm";
+    2nd iteration: str[1] = str[1].charAt(0).toUpperCase() + str[1].slice(1);
+                    str[1] = "a".charAt(0).toUpperCase()    + "a".slice(1);
+                    str[1] = "A"                            + "";
+                    str[1] = "A";
+    3rd iteration: str[2] = str[2].charAt(0).toUpperCase()   + str[2].slice(1);
+                    str[2] = "little".charAt(0).toUpperCase() + "little".slice(1);
+                    str[2] = "L"                              + "ittle";
+                    str[2] = "Little";
+    4th iteration: str[3] = str[3].charAt(0).toUpperCase() + str[3].slice(1);
+                    str[3] = "tea".charAt(0).toUpperCase()  + "tea".slice(1);
+                    str[3] = "T"                            + "ea";
+                    str[3] = "Tea";
+    5th iteration: str[4] = str[4].charAt(0).toUpperCase() + str[4].slice(1);
+                    str[4] = "pot".charAt(0).toUpperCase() + "pot".slice(1);
+                    str[4] = "P"                           + "ot";
+                    str[4] = "Pot";                                                         
+    End of the FOR Loop, still an array*/
+    }
+    
+    // Step 4. Return the output
+    return str.join(' '); // ["I'm", "A", "Little", "Tea", "Pot"].join(' ') => "I'm A Little Tea Pot"
 }
 
 titleCase("I'm a little tea pot");
 
+
+
+// GIVEN:
 // function titleCase(str) {
 //     return str;
 // }
 
 // titleCase("I'm a little tea pot");
+
+
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
-// So lets figure out what we are being asked to do: we need to return the provided string with each first letter capitalized per word. 
 
-// function titleCase(str) {
-//     const newTitle =  str.split(" ");
-//     const updatedTitle = [];
-//     for (let st in newTitle) {
-//         updatedTitle[st] = newTitle[st][0].toUppercase() + newTitle[st].slice(1).toLowerCase();
-//     }
-//     return updatedTitle.join(" ")
-// }
+// I am omitting the FCC answer as it glosses over core concepts and is a poorly written explanation.
 
-// titleCase("I'm a little tea pot");
 
-// Code Explanation
-
-// Split the string by white spaces, and create a variable to track the updated title. 
-// Then we use a loop to turn turn the first character of the word to uppercase and the rest to lowercase. 
-// by creating concatenated string composed of the whole word in lowercase with the first character replaced by its uppercase.
+// https://www.freecodecamp.org/news/three-ways-to-title-case-a-sentence-in-javascript-676a9175eb27/
+// https://forum.freecodecamp.org/t/let-in-javascript/498672
