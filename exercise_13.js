eab50ee78e7f8cf02304e566b802eac96130c4ac
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-17 21:21:45 -0800

    Update exercise_13.js

--- exercise_13.js
+++ exercise_13.js
@@ -1,6 +1,23 @@
+// Falsy Bouncer
+
+// Remove all falsy values from an array. Return a new array; do not mutate the original array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+// Hint: Try converting each value to a Boolean.
 
 
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+function bouncer(arr) {
+    return arr;
+}
 
+bouncer([7, "ate", "", false, 9]);
+
+// function bouncer(arr) {
+//     return arr;
+// }
+
+// bouncer([7, "ate", "", false, 9]);
 
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
