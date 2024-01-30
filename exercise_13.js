7e5a4ce22684f42bad49901fb30a33e458eb8326
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-01-29 19:13:37 -0800

    Update exercise_13.js

--- exercise_13.js
+++ exercise_13.js
@@ -27,7 +27,9 @@ bouncer([7, "ate", "", false, 9]);
 // function bouncer(arr) {
 //     const filteredArr = [];
 //     for (let i = 0; i < arr.length; i++) {
-//       if (arr[i]) filteredArr.push(arr[i]);
+//       if (arr[i])  {
+//          filteredArr.push(arr[i]);
+//        }
 //     }
 //     return filteredArr;
 //   }
