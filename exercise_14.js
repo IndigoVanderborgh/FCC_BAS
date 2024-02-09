dab56ef440a0d89dc09c8bb2713143534ba8f65c
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-02-08 19:36:34 -0800

    Update exercise_14.js

--- exercise_14.js
+++ exercise_14.js
@@ -9,7 +9,11 @@
 
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
 function getIndexToIns(arr, num) {
-    return num;
+    arr.sort((a, b) => a - b)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) return i;
+    }
+    return arr.length;
 }
 
 getIndexToIns([40, 60], 50);
