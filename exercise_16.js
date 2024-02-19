9660cfb9987a6c03429334b5f2b2d99779f781e2
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2024-02-18 17:35:16 -0800

    Update exercise_16.js

--- exercise_16.js
+++ exercise_16.js
@@ -2,28 +2,43 @@
 
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
-function chunkArrayInGroups(arr, size) {
-  let temp = []; // empty array 
-  const result = []; // empty array 
+// Original  
+// function chunkArrayInGroups(arr, size) {
+//     return arr;
+//   }
 
-  for (let a = 0; a < arr.length; a++) { // 
-    if (a % size !== size - 1) 
-      temp.push(arr[a]);
+//   chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+// Solution 1
+function chunkArrayInGroups(arr, size) {
+  let temp = []; // empty array which we will fill our chunk in
+  const result = []; // empty array with all array chunks in as sub arrays yielding a 2d array
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) // if i/size remainder is not equal to size - 1
+      temp.push(arr[i]); // push to temp
+
     else {
-      temp.push(arr[a]);
-      result.push(temp);
-      temp = [];
+      temp.push(arr[i]); // push to temp
+      result.push(temp); // push temp to result
+      temp = []; // reset temp to empty array. 
     }
+
   }
 
-  if (temp.length !== 0) result.push(temp);
-  return result;
-
+  if (temp.length !== 0) result.push(temp); // if temp is not an empty array, push to result.
+  return result; // return result
 }
 
-// OR
+// Another way to solve this:
 
-
+// function chunkArrayInGroups(arr, size) { 
+//   const newArr = []; // empty array which will be filled with our chunky chunks.
+//   for (let i = 0; i < arr.length; i += size) { // loop starts at zero, increments by size, and ends at the array length
+//     newArr.push(arr.slice(i, i + size)); // inside the loop, each chunk is created via (arr.slice(i, i + size)), where slice accepts two arguments (start, end), and is pushed to newArr.
+//   }
+//   return newArr; // once complete, we return newArr once complete.
+// }
 
 
 // function chunkArrayInGroups(arr, size) {
@@ -31,15 +46,3 @@ function chunkArrayInGroups(arr, size) {
 //   }
 
 //   chunkArrayInGroups(["a", "b", "c", "d"], 2);
-
-
-// Code Explanation
-
-//     Firstly, we create two empty arrays called temp and result, which we will eventually return.
-//     Our for loop loops until a is equal to or more than the length of the array in our test.
-//     Inside our loop, we push to temp using temp.push(arr[a]); if the remainder of a / size is not equal to size - 1.
-//     Otherwise, we push to temp, push temp to the result variable and reset temp to an empty array.
-//     Next, if temp isn’t an empty array, we push it to result.
-//     Finally, we return the value of result.
-
-// review day 2x
